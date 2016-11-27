9de4117bc475363b3a09f7627749f8059fc953f9
Steffen W <sateffen@gmail.com>
2016-11-27 12:23:32 +0100

    Implemented static file handler

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
 'use strict';
 
-const simpleMime = require('simple-mime')('application/octet-stream');
+const simpleMime = require('simple-mime');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = function (aRequest, aResponse) {
-    // todo: code
-}
+module.exports = function (aConfig) {
+    const defaultMimeType = typeof aConfig.defaultMimeType === 'string' ? aConfig.defaultMimeType : 'application/octet-stream';
+    const rootDir = typeof aConfig.root === 'string' && aConfig.root.length ? aConfig.root : process.cwd();
+    const indexFile = typeof aConfig.indexFile === 'string' && aConfig.indexFile.length ? aConfig.indexFile : 'index.html';
+    const notFoundHandler = typeof aConfig.notFoundHandler === 'function' ? aConfig.notFoundHandler : (aRequest, aResponse) => {
+        aResponse
+            .setStatusCode(404)
+            .write('Not Found handler');
+    };
+    const mimeFunction = simpleMime(defaultMimeType);
+
+    return (aRequest, aResponse) => {
+        if (aRequest.getMethod() !== 'GET' && aRequest.getMethod() !== 'HEAD') {
+            return notFoundHandler(aRequest, aResponse);
+        }
+
+        return new Promise((aResolve, aReject) => {
+            const target = aRequest.getRequestPath().replace(/\/[.]{1,2}/g, '/');
+            let targetPath = path.join(rootDir, target);
+
+            fs.stat(targetPath, (aError, aStat) => {
+                if (aError) {
+                    return aReject(aError);
+                }
+
+                if (aStat.isDirectory()) {
+                    targetPath = path.join(targetPath, indexFile);
+                }
+
+                aResponse
+                    .setStatusCode(200)
+                    .setHeader('Content-Type', mimeFunction(targetPath))
+                    .pipe(fs.createReadStream(targetPath));
+
+                return aResolve();
+            });
+        }).catch(() => {
+            return notFoundHandler(aRequest, aResponse);
+        });
+    };
+};
